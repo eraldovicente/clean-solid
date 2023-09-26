be6fcbb61a022ecfbdd424bff17de77c37dde6cb
eraldovicente <eraldovicente2@gmail.com>
2023-09-25 21:53:56 -0300

    Fim do curso de clean code e solid

---
+++ src/clean-code/06-classes-b.ts
@@ -0,0 +1,104 @@
+(() => {
+
+    // No aplicando el principio de responsabilidad única
+
+    type Gender = 'M'|'F';
+
+    interface PersonProps {
+        birthdate: Date;
+        gender   : Gender;
+        name     : string;
+    }
+
+    class Person {
+        public birthdate: Date;
+        public gender   : Gender;
+        public name     : string;
+
+        constructor({ name, gender, birthdate }: PersonProps ) {
+            this.name      = name;
+            this.gender    = gender;
+            this.birthdate = birthdate;
+        }
+    } 
+
+    interface UserProps {
+        birthdate: Date;
+        email: string;
+        gender: Gender;
+        name: string;
+        role: string;
+    }
+
+    class User extends Person {
+
+        public email: string;
+        public lastAccess: Date;
+        public role: string;
+
+        constructor({
+            birthdate,
+            email,
+            gender,
+            name,
+            role,
+        }: UserProps) {
+            super({ name, gender, birthdate });
+            this.lastAccess = new Date();
+            this.email = email;
+            this.role = role;
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    interface UserSettingsProps {
+            birthdate              : Date; 
+            email                  : string;
+            gender                 : Gender;
+            lastOpenFolder         : string;   
+            name                   : string;
+            role                   : string;
+            workingDirectory       : string;
+    }
+
+    class UserSettings extends User {
+
+        public workingDirectory: string;
+        public lastOpenFolder: string;  
+
+        constructor({
+            workingDirectory,
+            lastOpenFolder,   
+            email,
+            role,
+            name,
+            gender,
+            birthdate, 
+        }: UserSettingsProps){
+            super({ email, role, name, gender, birthdate });
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder   = lastOpenFolder
+        }
+    }
+
+    // const newPerson = new Person('Fernando','M', new Date('1985-10-21'));
+    // console.log({ newPerson });
+    
+    const userSettings = new UserSettings({
+        birthdate: new Date('1985-10-21'),
+        email: 'fernando@google.com',
+        gender: 'M',
+        lastOpenFolder: '/home',
+        name: 'Fernando',
+        role: 'Admin',
+        workingDirectory: '/usr/home',
+    });
+
+    // console.log({ UserSettings, areCredentialsValid: userSettings.checkCredentials() });
+    
+
+})()
